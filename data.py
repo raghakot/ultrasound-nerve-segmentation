776ec9cb8d09be70524d4edc72497d84ef071156
Ragha <ragha@outlook.com>
2017-01-21 20:50:00 -0800

    update data.py to perform cleaning

--- data.py
+++ data.py
@@ -1,9 +1,46 @@
 from __future__ import print_function
 
 import os
+import shutil
 import numpy as np
+
+import skimage.util
+import skimage.io
+import scipy.spatial.distance as spdist
+
+from collections import defaultdict
 from augmentation.ImageUtils import load_img, list_images
 from sklearn.cross_validation import train_test_split
+
+
+def _compute_img_hist(img):
+    # Divide the image in blocks and compute per-block histogram
+    blocks = skimage.util.view_as_blocks(img, block_shape=(20, 20))
+    img_hists = [np.histogram(block, bins=np.linspace(0, 1, 10))[0] for block in blocks]
+    return np.concatenate(img_hists)
+
+
+def _are_inconsistent(mask1, mask2):
+    has_mask1 = np.count_nonzero(mask1) > 0
+    has_mask2 = np.count_nonzero(mask2) > 0
+    return has_mask1 != has_mask2
+
+
+def _filter_inconsistent(imgs, masks):
+    hists = np.array(map(_compute_img_hist, imgs))
+    dists = spdist.squareform(spdist.pdist(hists, metric='cosine'))
+
+    # + eye because image will be similar to itself. We dont want to include those.
+    close_pairs = dists + np.eye(dists.shape[0]) < 0.008
+    close_ij = np.transpose(np.nonzero(close_pairs))
+
+    # Find inconsistent masks among duplicates
+    valids = np.ones(len(imgs), dtype=np.bool)
+    for i, j in close_ij:
+        if _are_inconsistent(masks[i], masks[j]):
+            valids[i] = valids[j] = False
+
+    return np.array(imgs)[valids], np.array(masks)[valids]
 
 
 class DataManager(object):
@@ -18,7 +55,7 @@ class DataManager(object):
 
     @staticmethod
     def read_train_images():
-        train_data_path = os.path.join(DataManager.DATA_PATH, 'train_cleaned')
+        train_data_path = os.path.join(DataManager.DATA_PATH, 'train')
         images = list_images(train_data_path)
         total = len(images) / 2
 
@@ -52,6 +89,31 @@ class DataManager(object):
         print('Creating train dataset...')
         mask_labels = [1 if np.count_nonzero(mask) > 0 else 0 for mask in imgs_mask]
         DataManager.save_train_val_split(imgs, imgs_mask, "all", stratify=mask_labels)
+
+    @staticmethod
+    def create_cleaned_train_data():
+        # Group by patient id.
+        patient_classes, imgs, imgs_mask = DataManager.read_train_images()
+
+        print("Cleaning bad training data...")
+        pid_data_dict = defaultdict(list)
+        for i, pid in enumerate(patient_classes):
+            pid_data_dict[pid].append((imgs[i], imgs_mask[i]))
+
+        imgs_cleaned = []
+        imgs_masks_cleaned = []
+        for pid in pid_data_dict:
+            imgs, masks = zip(*pid_data_dict[pid])
+            filtered_imgs, filtered_masks = _filter_inconsistent(imgs, masks)
+            print("Discarded {} from patient {}".format(len(imgs) - len(filtered_imgs), pid))
+            imgs_cleaned.extend(filtered_imgs)
+            imgs_masks_cleaned.extend(filtered_masks)
+
+        imgs = np.array(imgs_cleaned)
+        imgs_mask = np.array(imgs_masks_cleaned)
+        print("Creating cleaned train dataset: {} items".format(len(imgs)))
+        mask_labels = [1 if np.count_nonzero(mask) > 0 else 0 for mask in imgs_mask]
+        DataManager.save_train_val_split(imgs, imgs_mask, "cleaned", stratify=mask_labels)
 
     @staticmethod
     def create_test_data():
@@ -108,5 +170,5 @@ class DataManager(object):
 
 if __name__ == '__main__':
     dm = DataManager()
-    dm.create_train_data()
+    dm.create_cleaned_train_data()
     dm.create_test_data()
